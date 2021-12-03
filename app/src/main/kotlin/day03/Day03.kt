d79c7bd53174e9d12652c6331de495d31a21154a
Henning Bunk <henningtbunk@gmail.com>
2021-12-03 13:10:55 +0100

    use ints instead of chars

--- app/src/main/kotlin/day03/Day03.kt
+++ app/src/main/kotlin/day03/Day03.kt
@@ -13,39 +13,65 @@ fun main(args: Array<String>) {
 
 fun solveDay03Part1(input: List<String>): Int {
     val gamma = input
-        .map { it.toCharArray().toList() }
+        .map { it.toList().map(Character::getNumericValue) }
         .transpose()
         .map { it.findMostCommon() }
-        .joinToBitString().also { println(it) }
-
-    val epsilon = gamma.invertBits()
-
-    return gamma.toInt(2) * epsilon.toInt(2)
+    val epsilon = gamma.map { it xor 1 }
+    return gamma.toDecimal() * epsilon.toDecimal()
 }
 
 fun solveDay03Part2(input: List<String>): Int {
-    val inputAsCharList = input.map { it.toCharArray().toList() }
+    val inputAsIntList = input.map { it.toList().map(Character::getNumericValue) }
     val oxygenGeneratorRating = findRecursive(
-        input = inputAsCharList,
+        input = inputAsIntList,
         index = 0,
         criteria = ::oxygenGeneratorRatingBitCriteria
-    )
-        .joinToBitString()
-        .toInt(2)
+    ).toDecimal()
 
     val co2ScrubberRating = findRecursive(
-        input = inputAsCharList,
+        input = inputAsIntList,
         index = 0,
         criteria = ::co2ScrubberRatingBitCriteria
-    )
-        .joinToBitString()
-        .toInt(2)
+    ).toDecimal()
 
     println("Oxygen Generator Rating: $oxygenGeneratorRating")
     println("CO2 Scrubber Rating: $co2ScrubberRating")
     return oxygenGeneratorRating * co2ScrubberRating
 }
 
+fun List<Int>.findMostCommon(): Int =
+    if (count { it == 1 } >= count { it == 0 }) 1 else 0
+
+fun List<Int>.findLeastCommon(): Int =
+    if (count { it == 1 } >= count { it == 0 }) 0 else 1
+
+fun oxygenGeneratorRatingBitCriteria(bit: List<Int>, transposedDiagnosticReport: List<List<Int>>, index: Int): Boolean {
+    val mostCommonForIndex = transposedDiagnosticReport
+        .map { it.findMostCommon() }
+        .get(index)
+    return bit[index] == mostCommonForIndex
+}
+
+fun co2ScrubberRatingBitCriteria(bit: List<Int>, transposedDiagnosticReport: List<List<Int>>, index: Int): Boolean {
+    val leastCommonForIndex = transposedDiagnosticReport
+        .map { it.findLeastCommon() }
+        .get(index)
+    return bit[index] == leastCommonForIndex
+}
+
+fun findRecursive(
+    input: List<List<Int>>,
+    index: Int,
+    criteria: (List<Int>, List<List<Int>>, Int) -> Boolean,
+): List<Int> {
+    val transposedInput = input.transpose()
+    val filteredInput = input.filter { criteria(it, transposedInput, index) }
+
+    return when (filteredInput.size) {
+        1 -> filteredInput.first()
+        else -> findRecursive(filteredInput, index + 1, criteria)
+    }
+}
 
 fun <T> List<List<T>>.transpose(): List<List<T>> {
     val target: MutableList<List<T>> = emptyList<List<T>>().toMutableList()
@@ -55,43 +81,4 @@ fun <T> List<List<T>>.transpose(): List<List<T>> {
     return target
 }
 
-fun List<Char>.findMostCommon(): Char =
-    if (count { it == '1' } >= count { it == '0' }) '1' else '0'
-
-fun List<Char>.findLeastCommon(): Char =
-    if (count { it == '1' } >= count { it == '0' }) '0' else '1'
-
-fun String.invertBits(): String = toList()
-    .map { if (it == '0') '1' else '0' }
-    .joinToBitString()
-
-fun List<Char>.joinToBitString(): String = joinToString(separator = "")
-
-fun oxygenGeneratorRatingBitCriteria(bit: List<Char>, transposedDiagnosticReport: List<List<Char>>, index: Int): Boolean {
-    val mostCommonForIndex = transposedDiagnosticReport
-        .map { it.findMostCommon() }
-        .get(index)
-    return bit[index] == mostCommonForIndex
-}
-
-fun co2ScrubberRatingBitCriteria(bit: List<Char>, transposedDiagnosticReport: List<List<Char>>, index: Int): Boolean {
-    val leastCommonForIndex = transposedDiagnosticReport
-        .map { it.findLeastCommon() }
-        .get(index)
-    return bit[index] == leastCommonForIndex
-}
-
-fun findRecursive(
-    input: List<List<Char>>,
-    index: Int,
-    criteria: (List<Char>, List<List<Char>>, Int) -> Boolean,
-): List<Char> {
-    println("Index: $index, Size: ${input.size}")
-    val transposedInput = input.transpose()
-    val filteredInput = input.filter { criteria(it, transposedInput, index) }
-
-    return when (filteredInput.size) {
-        1 -> filteredInput.first()
-        else -> findRecursive(filteredInput, index + 1, criteria)
-    }
-}
+fun List<Int>.toDecimal() = joinToString(separator = "").toInt(2)
