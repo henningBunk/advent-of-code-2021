1b4012a27ed4b716e9b7eb1f39e1f1b472401b52
Henning Bunk <henningtbunk@gmail.com>
2021-12-08 20:46:02 +0100

    make it somewhat readable

--- app/src/main/kotlin/day08/Day08.kt
+++ app/src/main/kotlin/day08/Day08.kt
@@ -12,6 +12,7 @@ fun main(args: Array<String>) {
 }
 
 
+// The solution for day 8 part 1 is: 512
 fun solveDay08Part1(input: List<String>): Int = input
     .parseToDisplays()
     .map { it.fourDigitOutputValue }
@@ -23,142 +24,155 @@ fun solveDay08Part1(input: List<String>): Int = input
         }
     }
 
-fun solveDay08Part2(input: List<String>): Int {
-    return input
-        .parseToDisplays()
-        .map { display -> display.read() }
-        .sum()
-}
+// The solution for day 8 part 2 is: 1091165
+fun solveDay08Part2(input: List<String>): Int = input
+    .parseToDisplays()
+    .map { display -> display.read() }
+    .sum()
 
 data class Display(
     val tenUniqueSignalPatterns: List<String>,
     val fourDigitOutputValue: List<String>
 ) {
-    fun read(): Int {
 
+    private val wiring: Array<Char> = decode(tenUniqueSignalPatterns, fourDigitOutputValue)
+
+    fun read(): Int = fourDigitOutputValue
+        .map { it.getDigit(wiring) }
+        .joinToString("")
+        .toInt()
+
+    /**
+     * Decode the wiring by using an array of length 7.
+     * Each item of the array represents on segment of the 7 segment display
+     * Each item holds an array of chars, starting with abcdefg, representing all possibilities which code could be decoding this segment
+     * With few steps one after another we can reduce these possibilities until each item in the array will only contain one code.
+     * This is the correct wiring.
+     *
+     * The indices in the array represent the following segments:
+     *
+     *         0
+     *     ┏━━━━━━━┓
+     *     ┃       ┃
+     *   1 ┃       ┃ 2
+     *     ┃   3   ┃
+     *     ┣━━━━━━━┫
+     *     ┃       ┃
+     *   4 ┃       ┃ 5
+     *     ┃       ┃
+     *     ┗━━━━━━━┛
+     *         6
+     *
+     */
+    private fun decode(
+        tenUniqueSignalPatterns: List<String>,
+        fourDigitOutputValue: List<String>
+    ): Array<Char> {
         val data: List<Array<Char>> = (tenUniqueSignalPatterns + fourDigitOutputValue)
             .map { it.toCharArray().toTypedArray() }
         val possibleCandidates: Array<Array<Char>> = Array(7) { arrayOf('a', 'b', 'c', 'd', 'e', 'f', 'g') }
 
-        data.forEach { number: Array<Char> ->
-            when (number.size) {
-                // Distinct Numbers, must be 1, 4 or 7
-                2 -> possibleCandidates.narrowDownCandidates(number, 0, 1, 3, 4, 6)
-                3 -> possibleCandidates.narrowDownCandidates(number, 1, 3, 4, 6)
-                4 -> possibleCandidates.narrowDownCandidates(number, 0, 4, 6)
+        // Distinct Numbers 1, 4 or 7
+        data.forEach { numberCode: Array<Char> ->
+            when (numberCode.size) {
+                2 -> possibleCandidates.narrowDownCandidates(numberCode, 2, 5) // 1
+                3 -> possibleCandidates.narrowDownCandidates(numberCode, 0, 2, 5) // 7
+                4 -> possibleCandidates.narrowDownCandidates(numberCode, 1, 3, 2, 5) // 4
             }
         }
 
-        val sharedByFiveSegmentNumbers = data.filter { it.size == 5 }.let { fiveSegmentNumbers ->
-            // Five segment numbers, must be 2, 3, or 5
-            val appearsInAll: Array<Char> = fiveSegmentNumbers.fold(setOf('a', 'b', 'c', 'd', 'e', 'f', 'g')) { rest, next ->
+        // Five segment numbers 2, 3, or 5
+        val sharedByFiveSegmentNumbers = data
+            .filter { it.size == 5 }
+            .fold(setOf('a', 'b', 'c', 'd', 'e', 'f', 'g')) { rest, next ->
                 rest.intersect(next.toSet())
             }.toTypedArray()
-            // The shared ones must be the horizontal ones
-            possibleCandidates.narrowDownCandidates(appearsInAll, 1, 2, 4, 5)
-            appearsInAll
-        }
 
-        val sharedBySixSegmentNumbers = data.filter { it.size == 6 }
-            .onEach { it.sort() }
-            .distinct()
+        // The shared ones must be the horizontal ones
+        possibleCandidates.narrowDownCandidates(sharedByFiveSegmentNumbers, 0, 3, 6)
+
+        // Six segment numbers 0, 6, 9
+        val sharedBySixSegmentNumbers: Array<Char> = data
+            .filter { it.size == 6 }
             .map { it.toSet() }
             .let { sixSegmentNumbers ->
-                // Six segment numbers, must be 0, 6, 9
-                val appearsInAll = listOf(
-                    sixSegmentNumbers[1].union(sixSegmentNumbers[2]).minus(sixSegmentNumbers[0]).joinToString(""),
-                    sixSegmentNumbers[0].union(sixSegmentNumbers[2]).minus(sixSegmentNumbers[1]).joinToString(""),
-                    sixSegmentNumbers[0].union(sixSegmentNumbers[1]).minus(sixSegmentNumbers[2]).joinToString(""),
-                ).joinToString("")
-                appearsInAll
+                arrayOf(
+                    sixSegmentNumbers[1].union(sixSegmentNumbers[2]).minus(sixSegmentNumbers[0]).first(),
+                    sixSegmentNumbers[0].union(sixSegmentNumbers[2]).minus(sixSegmentNumbers[1]).first(),
+                    sixSegmentNumbers[0].union(sixSegmentNumbers[1]).minus(sixSegmentNumbers[2]).first(),
+                )
             }
 
-        val middleHorizontal: Char = sharedByFiveSegmentNumbers.toSet().intersect(sharedBySixSegmentNumbers.toSet()).first()
-        possibleCandidates.narrowDownCandidates(arrayOf(middleHorizontal).opposite, 3)
+        val middleHorizontal: Array<Char> = sharedByFiveSegmentNumbers
+            .toSet()
+            .intersect(sharedBySixSegmentNumbers.toSet())
+            .toTypedArray()
+        possibleCandidates.narrowDownCandidates(middleHorizontal, 3)
 
+        val sharedBySixSegmentNumbersWithoutHorizontal =
+            sharedBySixSegmentNumbers.filter { it != middleHorizontal.first() }
 
-        val restOfSixDigitNumbers = sharedBySixSegmentNumbers.filter { it != middleHorizontal }
-        if (possibleCandidates[2].contains(restOfSixDigitNumbers[1]) || possibleCandidates[5].contains(
-                restOfSixDigitNumbers[1]
-            )
+        if (possibleCandidates[2].contains(sharedBySixSegmentNumbersWithoutHorizontal[0]) &&
+            possibleCandidates[5].contains(sharedBySixSegmentNumbersWithoutHorizontal[0])
         ) {
-            possibleCandidates.removeCandidate(arrayOf(restOfSixDigitNumbers[1]).opposite, 2)
-            possibleCandidates.removeCandidate(arrayOf(restOfSixDigitNumbers[1]), 5)
-
-            possibleCandidates.removeCandidate(arrayOf(restOfSixDigitNumbers[0]).opposite, 4)
-            possibleCandidates.removeCandidate(arrayOf(restOfSixDigitNumbers[0]), 1)
+            // sharedBySixSegmentNumbersWithoutHorizontal[0] must be a 9 because it shares both segments with one
+            possibleCandidates.narrowDownCandidates(arrayOf(sharedBySixSegmentNumbersWithoutHorizontal[0]), 2)
+            // sharedBySixSegmentNumbersWithoutHorizontal[1] must be a 6
+            possibleCandidates.narrowDownCandidates(arrayOf(sharedBySixSegmentNumbersWithoutHorizontal[1]), 4)
         } else {
-
-            possibleCandidates.removeCandidate(arrayOf(restOfSixDigitNumbers[0]).opposite, 2)
-            possibleCandidates.removeCandidate(arrayOf(restOfSixDigitNumbers[0]), 5)
-
-            possibleCandidates.removeCandidate(arrayOf(restOfSixDigitNumbers[1]).opposite, 4)
-            possibleCandidates.removeCandidate(arrayOf(restOfSixDigitNumbers[1]), 1)
+            // sharedBySixSegmentNumbersWithoutHorizontal[0] must be a 6 because it shares both segments with one
+            possibleCandidates.narrowDownCandidates(arrayOf(sharedBySixSegmentNumbersWithoutHorizontal[0]), 4)
+            // sharedBySixSegmentNumbersWithoutHorizontal[1] must be a 9
+            possibleCandidates.narrowDownCandidates(arrayOf(sharedBySixSegmentNumbersWithoutHorizontal[1]), 2)
         }
 
-        evaluate(possibleCandidates)
+        // Bottom horizontal is the last one we didn't address, it should be unique though by this time.
 
-        // Bottom horizontal
-        val possibleCandidatesBottomHorizontal = possibleCandidates[6].toSet()
-        val possibleCandidatesRest = possibleCandidates.copyOfRange(0, 6).map { it[0] }.toSet()
-        val letter = possibleCandidatesBottomHorizontal.subtract(possibleCandidatesRest)
-
-
-        possibleCandidates.narrowDownCandidates(arrayOf(letter.first()).opposite, 6)
-        evaluate(possibleCandidates)
-
-
-        val wiring: CharArray = possibleCandidates.map { it.first() }.toCharArray()
-        return fourDigitOutputValue.map { it.getDigit(wiring) }.joinToString("").toInt()
+        return possibleCandidates.map { it.first() }.toCharArray().toTypedArray()
     }
 
-    private fun String.getDigit(wiring: CharArray): Int {
-        print("${wiring.toList()}, $this, ")
-        if (length == 2) return 1.also(::println)
-        if (length == 3) return 7.also(::println)
-        if (length == 4) return 4.also(::println)
-        if (length == 7) return 8.also(::println)
-        if (length == 5) {
-            //2, 3, 5
-            if (contains(wiring[1])) return 5.also(::println)
-            if (contains(wiring[4])) return 2.also(::println)
-            return 3.also(::println)
-        } else {
-            //0, 6, 9
-            if (!contains(wiring[3])) return 0.also(::println)
-            if (contains(wiring[2])) return 9.also(::println)
-            return 6.also(::println)
+    private fun String.getDigit(wiring: Array<Char>): Int = when (length) {
+        2 -> 1
+        3 -> 7
+        4 -> 4
+        7 -> 8
+        5 -> when { // 2, 3, 5
+            contains(wiring[1]) -> 5
+            contains(wiring[4]) -> 2
+            else -> 3
         }
-        return -1
+        else -> when { // 0, 6, 9
+            !contains(wiring[3]) -> 0
+            contains(wiring[2]) -> 9
+            else -> 6
+        }
+    }
+
+    /**
+     * Called on our array for the candidates. Two things happen:
+     *  1. Only the chars which are given as candidates parameter remain for the given segments.
+     *  2. The given chars are removed from all other segments
+     */
+    private fun Array<Array<Char>>.narrowDownCandidates(
+        candidates: Array<Char>,
+        vararg segments: Int
+    ) {
+        removeCandidate(candidates.opposite, *segments.toTypedArray().toIntArray())
+        removeCandidate(candidates, *segments.toList().opposite.toTypedArray().toIntArray())
     }
 
     private val Array<Char>.opposite: Array<Char>
         get() = "abcdefg".filterNot(::contains).toCharArray().toTypedArray()
 
-
-    private fun Array<Array<Char>>.narrowDownCandidates(number: Array<Char>, vararg segments: Int) {
-        removeCandidate(number, *segments.toTypedArray().toIntArray())
-        removeCandidate(
-            number.opposite,
-            *listOf(0, 1, 2, 3, 4, 5, 6).filterNot { segments.contains(it) }.toTypedArray().toIntArray()
-        )
-        evaluate(this)
-    }
-
+    private val List<Int>.opposite: List<Int>
+        get() = (0..6).filterNot { contains(it) }
 
     private fun Array<Array<Char>>.removeCandidate(number: Array<Char>, vararg segments: Int) {
         segments.forEach { segment ->
-            this[segment] = get(segment).filterNot(number::contains).toTypedArray()
+            this[segment] = this[segment].filterNot(number::contains).toTypedArray()
         }
     }
-
-    fun evaluate(possibleCandidates: Array<Array<Char>>) {
-        println("Goal      : [d, e, a, f, g, b, c]")
-        println("Candidates: ${possibleCandidates.toList()}")
-        println()
-    }
 }
-
 
 fun List<String>.parseToDisplays(): List<Display> = map { line ->
     line.split("|")
