514d657d377162fc599eda84edf4971c8c94132b
Henning Bunk <henningtbunk@gmail.com>
2021-12-18 11:12:42 +0100

    Begin of day 15

---
+++ app/src/y2021/day15/Day15.kt
@@ -0,0 +1,21 @@
+package y2021.day15
+
+import common.*
+import common.annotations.AoCPuzzle
+
+fun main(args: Array<String>) {
+    Day15().solveThem()
+}
+
+@AoCPuzzle(2021, 15)
+class Day15 : AocSolution {
+    override val answers = Answers(samplePart1 = 40, samplePart2 = -1)
+
+    override fun solvePart1(input: List<String>): Any {
+        TODO()
+    }
+
+    override fun solvePart2(input: List<String>): Any {
+        TODO()
+    }
+}
---
+++ app/src/y2021/day15/Day15Test.kt
@@ -0,0 +1,8 @@
+package y2021.day15
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+
+class Day15Test : FreeSpec({
+
+})
