6ae8edac6112c7d0de8deea596babe1e15e6ac71
Henning Bunk <henningtbunk@gmail.com>
2021-12-26 19:23:26 +0100

    Beginning Day 17

---
+++ app/src/y2021/day17/Day17.kt
@@ -0,0 +1,21 @@
+package y2021.day17
+
+import common.*
+import common.annotations.AoCPuzzle
+
+fun main(args: Array<String>) {
+    Day17().solveThem()
+}
+
+@AoCPuzzle(2021, 17)
+class Day17 : AocSolution {
+    override val answers = Answers(samplePart1 = -1, samplePart2 = -1)
+
+    override fun solvePart1(input: List<String>): Any {
+        TODO()
+    }
+
+    override fun solvePart2(input: List<String>): Any {
+        TODO()
+    }
+}
