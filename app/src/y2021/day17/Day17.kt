5991d7cfd360e571a9ca176f11e1b79373c9e293
Henning Bunk <henningtbunk@gmail.com>
2021-12-26 21:40:31 +0100

    Day 17

--- app/src/y2021/day17/Day17.kt
+++ app/src/y2021/day17/Day17.kt
@@ -3,6 +3,9 @@ package y2021.day17
 import common.*
 import common.annotations.AoCPuzzle
 import helper.Point
+import kotlin.math.absoluteValue
+import kotlin.math.max
+import kotlin.math.sign
 
 fun main(args: Array<String>) {
     Day17().solveThem()
@@ -10,15 +13,86 @@ fun main(args: Array<String>) {
 
 @AoCPuzzle(2021, 17)
 class Day17 : AocSolution {
-    override val answers = Answers(samplePart1 = -1, samplePart2 = -1)
+    override val answers = Answers(samplePart1 = 45, samplePart2 = 112, part1 = 5050, part2 = 2223)
 
     override fun solvePart1(input: List<String>): Any {
-        TODO()
+        val target = parseToTarget(input.first())
+
+        var maxY = 0
+
+        for (y in (0..100)) {
+            var yHasAHit = false
+            for(x in (0..100)) {
+                val candidate = Probe(Point(x, y))
+
+                while(candidate.isOnTrack(target)) {
+                    candidate.step()
+                    if(candidate.isInTarget(target)) {
+                        maxY = max(maxY, candidate.highestReachedY)
+                        yHasAHit = true
+                        break
+                    }
+                }
+                if(yHasAHit) break
+            }
+        }
+        return maxY
     }
 
     override fun solvePart2(input: List<String>): Any {
-        TODO()
+        val target = parseToTarget(input.first())
+
+        var hitCount = 0
+        for (y in (target.y.first..100)) {
+            for(x in (0..target.x.last)) {
+                val candidate = Probe(Point(x, y))
+                while(candidate.isOnTrack(target) && !candidate.isInTarget(target)) {
+                    candidate.step()
+                    if(candidate.isInTarget(target)) {
+                        hitCount++
+                        break
+                    }
+                }
+            }
+        }
+        return hitCount
+    }
+
+    private fun parseToTarget(input: String): Target {
+        val (xStart, xEnd, yStart, yEnd) = """target area: x=(-?\d+)..(-?\d+), y=(-?\d+)..(-?\d+)"""
+            .toRegex()
+            .find(input)
+            ?.destructured ?: error("Did not match")
+        return Target(x = (xStart.toInt()..xEnd.toInt()), y = (yStart.toInt()..yEnd.toInt()))
     }
 }
 
-class Probe(var velocity: Point)
+class Probe(private var velocity: Point) {
+
+    private var _position = Point(0, 0)
+    val position: Point
+        get() = _position
+
+    private var _highestReachedY = 0
+    val highestReachedY: Int
+        get() = _highestReachedY
+
+    fun step() {
+        _position += velocity
+        velocity = Point(velocity.x.decrease(), velocity.y - 1)
+        _highestReachedY = max(_highestReachedY, _position.y)
+    }
+
+    fun isInTarget(target: Target): Boolean =
+        position.x in target.x && position.y in target.y
+
+    fun isOnTrack(target: Target): Boolean =
+        position.y >= target.y.first
+
+    private fun Int.decrease(): Int = when {
+        this == 0 -> 0
+        else -> sign * (absoluteValue - 1)
+    }
+}
+
+data class Target(val x: IntRange, val y: IntRange)
--- app/src/y2021/day17/Day17Test.kt
+++ app/src/y2021/day17/Day17Test.kt
@@ -1,8 +1,102 @@
 package y2021.day17
 
+import helper.Point
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 
 class Day17Test : FreeSpec({
 
+    "A probe which is shot with 5,2" - {
+        "is at 0,0 after zero steps" {
+            val probe = Probe(Point(5, 2))
+            probe.position shouldBe Point(0, 0)
+        }
+
+        "is at 5,2 after one step" {
+            val probe = Probe(Point(5, 2))
+            repeat(1) { probe.step() }
+            probe.position shouldBe Point(5, 2)
+        }
+
+        "is at 9,3 after two steps" {
+            val probe = Probe(Point(5, 2))
+            repeat(2) { probe.step() }
+            probe.position shouldBe Point(9, 3)
+        }
+
+        "is at 12,3 after three steps" {
+            val probe = Probe(Point(5, 2))
+            repeat(3) { probe.step() }
+            probe.position shouldBe Point(12, 3)
+        }
+
+        "is at 14,2 after four steps" {
+            val probe = Probe(Point(5, 2))
+            repeat(4) { probe.step() }
+            probe.position shouldBe Point(14, 2)
+        }
+
+        "is at 15,0 after five steps" {
+            val probe = Probe(Point(5, 2))
+            repeat(5) { probe.step() }
+            probe.position shouldBe Point(15, 0)
+        }
+
+        "is at 15,-3 after six steps" {
+            val probe = Probe(Point(5, 2))
+            repeat(6) { probe.step() }
+            probe.position shouldBe Point(15, -3)
+        }
+    }
+
+    "A probe with velocity 7,2 and a target of x=20..30, y=-10..-5" - {
+
+        val target = Target((20..30), (-10..-5))
+
+        "is not in the target after 6 steps" {
+            val probe = Probe(Point(7, 2))
+            repeat(6) { probe.step() }
+            probe.isInTarget(target) shouldBe false
+        }
+
+        "is in the target after 7 steps" {
+            val probe = Probe(Point(7, 2))
+            repeat(7) { probe.step() }
+            probe.isInTarget(target) shouldBe true
+        }
+    }
+
+    "A probe with velocity 7,-1 and a target of x=20..30, y=-10..-5" - {
+
+        val target = Target((20..30), (-10..-5))
+
+        "should hit the target" {
+            val probe = Probe(Point(7, -1))
+            while (probe.isOnTrack(target) && !probe.isInTarget(target)) {
+                probe.step()
+                println(probe.position)
+            }
+            probe.isInTarget(target) shouldBe true
+        }
+    }
+
+    "A probe with velocity 6,0 and a target of x=20..30, y=-10..-5" - {
+
+        val target = Target((20..30), (-10..-5))
+
+        "should hit the target" {
+            val probe = Probe(Point(6, 0))
+            while (probe.isOnTrack(target) && !probe.isInTarget(target)) {
+                probe.step()
+                println(probe.position)
+            }
+            probe.isInTarget(target) shouldBe true
+        }
+    }
+
+    "A probe with velocity 6,9 reaches the highest point of 45" {
+        val probe = Probe(Point(6, 9))
+        repeat(50) { probe.step(); println(probe.position.y) }
+        probe.highestReachedY shouldBe 45
+    }
 })
